c5d05c41d86812c11b7b3a07136f29fc0cbbe8b6
Juli Emmel <justjiffy@gmail.com>
2015-10-26 14:23:18 -0700

    changing order of routes to correct deployment issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'calendar/' => 'calendar#index', as: :calendar_path
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
+
+  get 'calendar/' => 'calendar#index', as: :calendar_path
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
