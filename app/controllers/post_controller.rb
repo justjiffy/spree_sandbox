ab9b7d2d2bdef46a6f52586d07de2f54df333e3b
Juli Emmel <justjiffy@gmail.com>
2015-10-28 11:25:12 -0700

    adding posts to homepage

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,63 @@
 class PostController < ApplicationController
+ 
+  def index
+    @post = Post.all.sort_by(&:created_at)
+  end
+
+  def show
+    @posts = Post.all.sort_by(&:created_at)
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.where(:post_id => @post)
+  end
+
+  def new
+    @posts = Post.all 
+  end
+
+  def new_post
+    @post = Post.new
+  end
+
+  def create
+  	@post = Post.create(post_params)
+  	if @post.save
+  		redirect_to post_path
+  	else redirect_to root_path
+  	end
+  end
+
+  def edit
+  	@edit_post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update_attributes(post_params)
+  		redirect_to post_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def all
+  	@posts = Post.all
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+  	redirect_to post_path
+  end
+  
+    private
+
+    def post_params
+      params.require(:post).permit(:user_id, :title, :body, :img, :tags)
+    end
+
+
+end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < Spree::BaseController
 
 	def index
+		@posts = Post.all.sort_by(&:created_at)
 		render layout: '../views/spree/layouts/spree_application'
 	end
 
