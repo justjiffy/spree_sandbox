4c61cbbdd07efa8db115ddec633bb7ad3ea0dda4
Juli Emmel <justjiffy@gmail.com>
2015-10-27 21:49:10 -0700

    backend template matches frontend!

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( spree/frontend/custom_app.js )
+
+# Changes Logo
+Rails.application.config.assets.precompile += %w( logo/spree_50.png )
