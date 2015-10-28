e1aaf098b0aa3bc40b5e5d30ba910dc8c4fb4ba8
Juli Emmel <justjiffy@gmail.com>
2015-10-28 13:41:43 -0700

    adding back button glyphs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,52 @@ class WelcomeController < Spree::BaseController
 
 	def show
 		@posts = Post.all.sort_by(&:created_at)
-		# @post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 	end
 
-end
+	def new
+		@posts = Post.all 
+	end
+
+	def new_post
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(post_params)
+		if @post.save
+			redirect_to post_path
+		else redirect_to root_path
+		end
+	end
+
+	def edit
+		@edit_post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			redirect_to post_path
+		else
+			render :edit
+		end
+	end
+
+	def all
+		@posts = Post.all
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to post_path
+	end
+
+			private
+
+			def post_params
+				params.require(:post).permit(:user_id, :title, :body, :img, :tags)
+			end
+
+	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
-  get '/' => 'welcome#index'
+  get '/' => 'welcome#index', as: :root
   
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
