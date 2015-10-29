9d013bceac8c6c8163e7435c68322f85d88687f2
Juli Emmel <justjiffy@gmail.com>
2015-10-29 16:39:08 -0700

    removing iframe allowance code from application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Enable iFrame
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,6 +21,7 @@ module Store
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
